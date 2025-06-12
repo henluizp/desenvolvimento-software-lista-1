1908227ed83173398ebc49e5454495dec8c8c431
Henrique <henrique.pintoluiz@gmail.com>
2025-06-12 20:00:46 -0300

    Exercise one

---
+++ exercise_one.py
@@ -0,0 +1,8 @@
+age_years = int(input("Digite sua idade em anos: "))
+
+days = age_years * 365
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+
+print(f"{days} dias \n{hours} horas \n{minutes} minutos \n{seconds} segundos")
